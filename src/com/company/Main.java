b6104855bef9aab7dd4bda7aece8bda009355172
Erik <erik@bigdon.de>
2020-11-24 08:27:15 -0800

    Auto stash before merge of "main" and "origin/main"
    Working Database connection

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,29 @@ import java.sql.*;
 public class Main {
 
     public static void main(String[] args) {
-        
+            String url = "jdbc:mysql://localhost";
+            String username = "dbi";
+            String password = "dbi_pass";
+
+            System.out.println("Connecting database...");
+
+            try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost", username, password)) {
+                System.out.println("Database connected!");
+            } catch (SQLException e) {
+                throw new IllegalStateException("Cannot connect the database!", e);
+            }
+    }
+/*
+    public static void close_connection(Connection con){
+        con.close();
+    }*/
+
+    public static Connection connect_to_database(String url, String username,String password){
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            System.out.println("Database connected!");
+            return connection;
+        } catch (SQLException e) {
+            throw new IllegalStateException("Cannot connect the database!", e);
+        }
     }
 }
